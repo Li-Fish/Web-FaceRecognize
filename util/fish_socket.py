745a08df29e908b1620612683194d84683d71128
Fish <1136727300@qq.com>
2020-04-24 18:16:24 +0800

    完成自定义Socket，测试可用

--- util/fish_socket.py
+++ util/fish_socket.py
@@ -10,7 +10,7 @@ class ServerSocket():
         self.listen_thread = None
         self.ip = ip
         self.port = port
-        self.open_socket_list = Queue()
+        self.open_socket_list = Queue(1024)
 
     def listen(self):
         serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -26,6 +26,8 @@ class ServerSocket():
             serversocket.settimeout(5)
             try:
                 (clientsocket, address) = serversocket.accept()
+                log.info("accept client socket {}, queue size is {}".format(address, self.open_socket_list.qsize()))
+                self.open_socket_list.put(ClientSocket(clientsocket))
             except socket.timeout:
                 pass
 
@@ -37,3 +39,50 @@ class ServerSocket():
         self.is_listen = False
         if self.listen_thread is not None:
             self.listen_thread.join()
+
+
+class ClientSocket():
+    def __init__(self, raw_socket):
+        self.socket = raw_socket
+
+    def recv(self):
+        data = []
+
+        lenData = self.socket.recv(4)
+        len_data = int(lenData[0]) * (2 ** 24) + int(lenData[1]) * (2 ** 16) + \
+                   int(lenData[2]) * (2 ** 8) + int(lenData[3]) * (2 ** 0)
+
+        log.info("receive data len {}".format(len_data))
+
+        lenSum = 0
+        while True:
+            chunk = self.socket.recv(2048)
+            if len(chunk) == 0:
+                break
+            data.append(chunk)
+            lenSum += len(chunk)
+            if lenSum == len_data:
+                break
+
+        if lenSum != len_data:
+            raise RuntimeError("接受长度不符合预期")
+
+        return b''.join(data)
+
+    def raw_recv(self, buf_size=2048):
+        return self.socket.recv(buf_size)
+
+    def raw_send(self, data):
+        self.socket.send(data)
+
+    def close(self):
+        self.socket.close()
+
+
+if __name__ == '__main__':
+    test = ServerSocket("192.168.1.136", 11234)
+    test.start_listen()
+    while True:
+        s = test.open_socket_list.get()
+        data = s.recv()
+        print(data)
